a9f72f876052869c47e550a5ae896480975f799b
OPISTO\matti.yla-mononen <ylammatti@gmail.com>
2016-12-05 12:42:39 +0200

    Added start-screen with start,quit and help buttons

---
+++ IFM project/Assets/LoadSceneScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour {
+
+    public void LoadByIndex(int sceneIndex) 
+        {
+        SceneManager.LoadScene(sceneIndex);
+
+
+    }
+
+	
+	
+	}
+
